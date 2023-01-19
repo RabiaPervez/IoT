ea4f91f8ceb7417abfcb52b8cfaa74bd7c11ed6e
RabiaPervez <biapervez15@gmail.com>
2023-01-19 19:00:34 +0500

    Create 1-ShadowingAndMutabilityInVariables.rs

---
+++ RUST/1-ShadowingAndMutabilityInVariables.rs
@@ -0,0 +1,14 @@
+fn main(){
+    println!("Hello, world!");
+    let x = 5;
+    println!("The value of x is: {}", x);
+    let x=6; // only x = 6 will give compile error
+    println!("The value of x is: {}", x);
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x=6; // by mentioning mut, we can change value of variable x
+    println!("The value of x is: {}", x);
+
+
+}
